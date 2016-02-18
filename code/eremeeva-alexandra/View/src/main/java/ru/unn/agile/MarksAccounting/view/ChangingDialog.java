21f01211f8bfebbc140a49b6d9b390949f729f4f
eremeeva-alexandra <eremeevaaa94@mail.ru>
2016-02-18 18:34:31 +0300

    Changed View

--- code/eremeeva-alexandra/View/src/main/java/ru/unn/agile/MarksAccounting/view/ChangingDialog.java
+++ code/eremeeva-alexandra/View/src/main/java/ru/unn/agile/MarksAccounting/view/ChangingDialog.java
@@ -5,6 +5,7 @@ import ru.unn.agile.MarksAccounting.viewmodel.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.text.ParseException;
+import java.util.List;
 
 public class ChangingDialog extends JDialog {
     private DialogViewModel dialogViewModel;
@@ -23,11 +24,12 @@ public class ChangingDialog extends JDialog {
     private JLabel groupLabel;
     private JPanel inputPanel;
     private JPanel datePanel;
+    private JList<String> logList;
 
     public ChangingDialog(final TableOfMarks tableOfMarks,
-                          final DialogType typeOfDialog) {
+                          final DialogType typeOfDialog, final ILogger logger) {
         dialogTitle.setText(typeOfDialog.getTypeDescription());
-        initDialogViewModel(tableOfMarks, typeOfDialog);
+        initDialogViewModel(tableOfMarks, typeOfDialog, logger);
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
@@ -54,17 +56,57 @@ public class ChangingDialog extends JDialog {
             }
         });
 
+        subjectComboBox.addItemListener(new ItemListener() {
+            public void itemStateChanged(final ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    bind();
+                }
+            }
+        });
+
+        dateField.addFocusListener(new FocusListener() {
+            public void focusGained(final FocusEvent e) {
+                dateField.getHeight();
+            }
+
+            public void focusLost(final FocusEvent e) {
+                bind();
+                backBind();
+            }
+        });
+
+        inputTextField.addFocusListener(new FocusListener() {
+            public void focusGained(final FocusEvent e) {
+                inputTextField.getHeight();
+            }
+
+            public void focusLost(final FocusEvent e) {
+                bind();
+                backBind();
+            }
+        });
+
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
                 bind();
                 try {
                     ChangingDialog.this.dialogViewModel.changeTableOfMarks();
+                    backBind();
                     dispose();
                 } catch (NumberFormatException exception) {
+                    ChangingDialog.this.dialogViewModel.logErrorWhileChangingTable(
+                            "Mark is not an integer!");
+                    backBind();
                     JOptionPane.showMessageDialog(null, "Mark must be an integer value!");
                 } catch (ParseException exception) {
+                    ChangingDialog.this.dialogViewModel.logErrorWhileChangingTable(
+                            "Wrong format of date!");
+                    backBind();
                     JOptionPane.showMessageDialog(null, "Wrong format of date!");
                 } catch (RuntimeException exception) {
+                    ChangingDialog.this.dialogViewModel.logErrorWhileChangingTable(
+                            exception.getMessage());
+                    backBind();
                     JOptionPane.showMessageDialog(null, exception.getMessage());
                 }
             }
@@ -72,6 +114,7 @@ public class ChangingDialog extends JDialog {
 
         buttonCancel.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
+                ChangingDialog.this.dialogViewModel.logCancelledChangingTable();
                 dispose();
             }
         });
@@ -79,12 +122,14 @@ public class ChangingDialog extends JDialog {
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(final WindowEvent e) {
+                ChangingDialog.this.dialogViewModel.logCancelledChangingTable();
                 dispose();
             }
         });
 
         contentPane.registerKeyboardAction(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
+                ChangingDialog.this.dialogViewModel.logCancelledChangingTable();
                 dispose();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
@@ -110,34 +155,37 @@ public class ChangingDialog extends JDialog {
         studentLabel.setVisible(dialogViewModel.isDialogStudentBoxVisible());
         subjectLabel.setVisible(dialogViewModel.isDialogSubjectBoxVisible());
         dateLabel.setVisible(dialogViewModel.isDialogDateTextBoxVisible());
+        List<String> log = dialogViewModel.getLog();
+        String[] items = log.toArray(new String[log.size()]);
+        logList.setListData(items);
     }
 
     private void initDialogViewModel(final TableOfMarks tableOfMarks,
-                                     final DialogType typeOfDialog) {
+                                     final DialogType typeOfDialog, final ILogger logger) {
         switch (typeOfDialog) {
             case ADD_GROUP:
-                dialogViewModel = new AddGroupDialogViewModel(tableOfMarks);
+                dialogViewModel = new AddGroupDialogViewModel(tableOfMarks, logger);
                 break;
             case ADD_STUDENT:
-                dialogViewModel = new AddStudentDialogViewModel(tableOfMarks);
+                dialogViewModel = new AddStudentDialogViewModel(tableOfMarks, logger);
                 break;
             case ADD_SUBJECT:
-                dialogViewModel = new AddSubjectDialogViewModel(tableOfMarks);
+                dialogViewModel = new AddSubjectDialogViewModel(tableOfMarks, logger);
                 break;
             case ADD_MARK:
-                dialogViewModel = new AddMarkDialogViewModel(tableOfMarks);
+                dialogViewModel = new AddMarkDialogViewModel(tableOfMarks, logger);
                 break;
             case DELETE_GROUP:
-                dialogViewModel = new DeleteGroupDialogViewModel(tableOfMarks);
+                dialogViewModel = new DeleteGroupDialogViewModel(tableOfMarks, logger);
                 break;
             case DELETE_STUDENT:
-                dialogViewModel = new DeleteStudentDialogViewModel(tableOfMarks);
+                dialogViewModel = new DeleteStudentDialogViewModel(tableOfMarks, logger);
                 break;
             case DELETE_SUBJECT:
-                dialogViewModel = new DeleteSubjectDialogViewModel(tableOfMarks);
+                dialogViewModel = new DeleteSubjectDialogViewModel(tableOfMarks, logger);
                 break;
             case DELETE_MARK:
-                dialogViewModel = new DeleteMarkDialogViewModel(tableOfMarks);
+                dialogViewModel = new DeleteMarkDialogViewModel(tableOfMarks, logger);
                 break;
             default:
                 break;
--- code/eremeeva-alexandra/View/src/main/java/ru/unn/agile/MarksAccounting/view/TableOfMarks.java
+++ code/eremeeva-alexandra/View/src/main/java/ru/unn/agile/MarksAccounting/view/TableOfMarks.java
@@ -1,13 +1,20 @@
 package ru.unn.agile.MarksAccounting.view;
 
+import ru.unn.agile.MarksAccounting.infrastructure.CSVLogger;
+import ru.unn.agile.MarksAccounting.infrastructure.TableReader;
+import ru.unn.agile.MarksAccounting.infrastructure.TableSaver;
 import ru.unn.agile.MarksAccounting.viewmodel.DialogType;
 import ru.unn.agile.MarksAccounting.viewmodel.MainFormViewModel;
 import javax.swing.*;
+import javax.swing.filechooser.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public final class TableOfMarks {
     private MainFormViewModel mainFormViewModel;
@@ -23,24 +30,28 @@ public final class TableOfMarks {
     private JButton changingButton;
     private JTable dataTable;
     private JPanel changingPanel;
+    private JButton saveButton;
+    private JButton saveAsButton;
+    private JList<String> logList;
+    private JButton openButton;
+    private File currentFile;
 
     private TableOfMarks() { }
 
     private TableOfMarks(final MainFormViewModel mainFormViewModel) {
         this.mainFormViewModel = mainFormViewModel;
-        backBind();
+        currentFile = null;
         changingComboBox.setModel(mainFormViewModel.getChangingComboBoxModel());
+        TableOfMarks.this.mainFormViewModel.logActionChanging("add group");
+        backBind();
 
         changingButton.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
                 bind();
+                TableOfMarks.this.mainFormViewModel.logDialogActivating();
                 makeDialog(DialogType.getType(changingComboBox.getSelectedItem().toString()));
-                changingComboBox.setModel(
-                        TableOfMarks.this.mainFormViewModel.getChangingComboBoxModel());
-                groupsComboBox.setModel(
-                        TableOfMarks.this.mainFormViewModel.getGroupComboBoxModel());
-                subjectsComboBox.setModel(
-                        TableOfMarks.this.mainFormViewModel.getSubjectComboBoxModel());
+                TableOfMarks.this.mainFormViewModel.logDialogDeactivating();
+                updateComboBoxes();
                 backBind();
             }
         });
@@ -64,11 +75,59 @@ public final class TableOfMarks {
                 }
             }
         });
+
+        changingComboBox.addItemListener(new ItemListener() {
+            public void itemStateChanged(final ItemEvent event) {
+                if (event.getStateChange() == ItemEvent.SELECTED) {
+                    TableOfMarks.this.mainFormViewModel.logActionChanging(
+                            changingComboBox.getSelectedItem().toString());
+                    backBind();
+                }
+            }
+        });
+
+        saveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent event) {
+                if (currentFile == null) {
+                    chooseFileForSaving();
+                }
+                saveTable();
+            }
+        });
+
+        saveAsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent event) {
+                chooseFileForSaving();
+                saveTable();
+            }
+        });
+
+        openButton.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Text file", "txt");
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setFileFilter(filter);
+                if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    currentFile = fileChooser.getSelectedFile();
+                }
+                openTable();
+            }
+        });
     }
 
     public static void main(final String[] args) {
+        String logPath = "./TableOfMarksLog";
+        String ext = ".csv";
+        File logFile = new File(logPath + ext);
+        long i = 0;
+        while (logFile.exists()) {
+            logPath = "./TableOfMarksLog" + Long.toString(i);
+            logFile = new File(logPath + ext);
+            i++;
+        }
+        CSVLogger logger = new CSVLogger(logPath + ext);
         JFrame frame = new JFrame("Table of marks");
-        frame.setContentPane(new TableOfMarks(new MainFormViewModel()).mainPanel);
+        frame.setContentPane(new TableOfMarks(new MainFormViewModel(logger)).mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -84,13 +143,63 @@ public final class TableOfMarks {
         tableModel = mainFormViewModel.getTableModel();
         dataTable.setModel(tableModel);
         dataTable.setTableHeader(null);
+
+        List<String> log = mainFormViewModel.getLog();
+        String[] items = log.toArray(new String[log.size()]);
+        logList.setListData(items);
     }
 
     private void makeDialog(final DialogType dialogType) {
         ChangingDialog changingDialog = new ChangingDialog(mainFormViewModel.getTableOfMarks(),
-                dialogType);
+                dialogType, mainFormViewModel.getLogger());
         changingDialog.pack();
         changingDialog.setVisible(true);
+    }
+
+    private void chooseFileForSaving() {
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Text file", "txt");
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(filter);
+        if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+            currentFile = fileChooser.getSelectedFile();
+        }
+    }
+
+    private void saveTable() {
+        if (currentFile != null) {
+            try {
+                TableSaver.save(currentFile.toString(),
+                        TableOfMarks.this.mainFormViewModel.getTableOfMarks());
+                TableOfMarks.this.mainFormViewModel.logTableSaving(currentFile.toString());
+                backBind();
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Can't save file!");
+            }
+        }
+    }
+
+    private void openTable() {
+        if (currentFile != null) {
+            try {
+                TableOfMarks.this.mainFormViewModel.setTableOfMarks(TableReader.readFile(
+                        currentFile.toString()));
+                TableOfMarks.this.mainFormViewModel.logTableOpening(currentFile.toString());
+                updateComboBoxes();
+                TableOfMarks.this.mainFormViewModel.logActionChanging("add group");
+                backBind();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Can't open file!");
+            }
+        }
+    }
+
+    private void updateComboBoxes() {
+        changingComboBox.setModel(
+                TableOfMarks.this.mainFormViewModel.getChangingComboBoxModel());
+        groupsComboBox.setModel(
+                TableOfMarks.this.mainFormViewModel.getGroupComboBoxModel());
+        subjectsComboBox.setModel(
+                TableOfMarks.this.mainFormViewModel.getSubjectComboBoxModel());
     }
 
     private void createUIComponents() {
